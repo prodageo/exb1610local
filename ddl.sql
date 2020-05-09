5c682599d16b9eb3d75a0b22abe40186da2df030
frederic-baucher <frederic.baucher@insa-rouen.fr>
2020-05-09 11:51:46 +0200

    Update ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -1,1 +1,28 @@
--- inserer dans ce fichier le script SQL exporte depuis GenMyModel
+-- inserer dans ce fichier le script SQL exporte depuis GenMyModel, aprèsa voir modifier les champs id du type INTEGER en SERIAL
+
+-- DEBUT SCRIPT
+CREATE TABLE IF NOT EXISTS Personnes
+(
+    id SERIAL NOT NULL,
+    nom VARCHAR(70),
+    prenom VARCHAR(70),
+    fk_ville INTEGER,
+    PRIMARY KEY(id)
+);
+ 
+CREATE TABLE IF NOT EXISTS villes
+(
+    id SERIAL NOT NULL,
+    nom VARCHAR(70),
+    code_postal INTEGER,
+    PRIMARY KEY(id)
+);
+ 
+ 
+-- Create FKs
+ALTER TABLE Personnes
+    ADD    FOREIGN KEY (fk_ville)
+    REFERENCES villes(id)
+    MATCH SIMPLE
+;
+-- FIN SCRIPT
